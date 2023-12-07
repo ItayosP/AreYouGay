5cdad088bed972651229e4f4412678ea8c540ef1
ItayosP <itaypollack3@gmail.com>
2023-12-07 16:32:03 +0200

    done but buggy, ill fix later

--- Main.java
+++ Main.java
@@ -1,31 +1,71 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
         JFrame frame = new JFrame();
         JLabel label = new JLabel();
-        JButton button1 = new JButton();
-        JButton button2 = new JButton();
+        JButton buttonYes = new JButton();
+        JButton buttonNo = new JButton();
 
-        frame.setSize(220, 120);
-        frame.setVisible(true);
-        frame.setTitle("HAHA");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        buttonYes.addActionListener(e -> {
+            label.setText("I knew it");
+        });
 
-        label.setBounds(20, 20, 220, 20);
-        label.setVisible(true);
 
-        button1.setBounds(220-100, 120-90, 70, 20);
-        button1.setText("Yes");
-        button1.setVisible(true);
+        while (true) {
 
-        button2.setBounds(220-205, 120-90, 70, 20);
-        button2.setText("No");
-        button2.setVisible(true);
+            int randX = rand.nextInt(1920);
+            int randY = rand.nextInt(1080);
 
-        frame.add(label);
-        frame.add(button1);
-        frame.add(button2);
+            frame.setSize(220, 120);
+            frame.setVisible(true);
+            frame.setTitle("HAHA");
+            frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            frame.setLayout(null);
+
+            label.setBounds(20, 20, 220, 20);
+            label.setVisible(true);
+
+            buttonYes.setBounds(220 - 100, 120 - 90, 70, 20);
+            buttonYes.setText("Yes");
+            buttonYes.setVisible(true);
+
+            buttonNo.setBounds(220 - 205, 120 - 90, 70, 20);
+            buttonNo.setText("No");
+            buttonNo.setVisible(true);
+
+            buttonNo.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    Point mousePos = MouseInfo.getPointerInfo().getLocation();
+                    Point buttonNoPos = buttonNo.getLocationOnScreen();
+                    int buttonWidth = buttonNo.getWidth();
+                    int buttonHeight = buttonNo.getHeight();
+
+                    if (mousePos.x >= buttonNoPos.x && mousePos.x <= buttonNoPos.x + buttonWidth &&
+                            mousePos.y >= buttonNoPos.y && mousePos.y <= buttonNoPos.y + buttonHeight) {
+                        // Get screen bounds
+                        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                        Rectangle screenBounds = ge.getMaximumWindowBounds();
+
+                        int maxX = screenBounds.width - frame.getWidth();
+                        int maxY = screenBounds.height - frame.getHeight();
+
+                        int newX = Math.max(0, Math.min(randX, maxX));
+                        int newY = Math.max(0, Math.min(randY, maxY));
+
+                        frame.setLocation(newX, newY);
+                    }
+                }
+            });
+
+            frame.add(label);
+            frame.add(buttonYes);
+            frame.add(buttonNo);
+        }
     }
 }
